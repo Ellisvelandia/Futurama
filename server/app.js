d76c06b3b49e68f39a765525c1e86278ed68dc8e
Ellisvelandia <eyis619l@gmail.com>
2023-01-23 07:00:43 -0500

    render port api call

--- server/app.js
+++ server/app.js
@@ -4,11 +4,26 @@ const cors = require("cors");
 const futuramaRoute = require("./routes/futuramaRoutes");
 require('dotenv').config();
 
-require("./connection/conn")
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", futuramaRoute);
 
-app.listen(8080, () => {
-  console.log("SERVER STARTED SUCCESFULLY");
+app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'success from Futurama!',
+  });
 });
+
+const startServer = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL).then(() => console.log("mongoDB connected"));
+    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'))
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();
