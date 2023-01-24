d1f264db50b396a253f8e533c34211641d9fe1da
Ellisvelandia <eyis619l@gmail.com>
2023-01-24 10:41:59 -0500

    Videos-h

--- client/src/pages/EpisodeVideo.jsx
+++ client/src/pages/EpisodeVideo.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const EpisodeVideo = () => {
   const [videos, setVideos] = useState({});
@@ -21,8 +21,8 @@ const EpisodeVideo = () => {
   }, [_id]);
 
   return (
-    <div className="w-full flex flex-col justify-center textShadows">
-      <h1 className="text-white mt-4 w-full  md:text-5xl text-2xl">
+    <div className="w-full flex flex-col justify-center textShadows without">
+      <h1 className="text-white mt-4 w-full md:text-5xl text-2xl">
         Episode: {videos.title}
       </h1>
       <p className="text-3xl font-bold text-white">{videos.number}</p>
@@ -37,6 +37,14 @@ const EpisodeVideo = () => {
         allowFullScreen
         className="md:aspect-video aspect-square lg:h-[580px] lg:mt-2 mt-16 md:px-20 w-full"
       ></iframe>
+      <div className="flex justify-center my-1">
+        <Link
+          to="/episode"
+          className="flex bg-[#459ED3] py-2 md:mb-0 justify-center px-6 rounded mt-2 text-white hover:bg-cyan-600 drop-shadow-2xl transition-all duration-200 active:scale-90"
+        >
+          &larr; Back
+        </Link>
+      </div>
     </div>
   );
 };
