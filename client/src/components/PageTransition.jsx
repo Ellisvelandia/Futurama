4932ce2775193f35a29d993525f108aac1231bf6
Ellisvelandia <eyis619l@gmail.com>
2023-02-05 00:22:31 -0500

    transition pages effect

--- client/src/components/PageTransition.jsx
+++ client/src/components/PageTransition.jsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
 
 const opacityAnimation = {
-  initial: { opacity: 0.9 },
+  initial: { opacity: 0.6 },
   animate: { opacity: 1 },
-  exit: { opacity: 0.9 },
+  exit: { opacity: 0.6 },
 };
 
 const PageTransition = ({ children }) => {
